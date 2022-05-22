6531c319b1c2aa3665a597463f0948fd00bcd2d8
류정민 <37644126+charmdew@users.noreply.github.com>
2022-05-22 19:24:44 +0900

    [Gold V] Title: MooTube (Silver), Time: 3580 ms, Memory: 135180 KB -BaekjoonHub

--- 백준/Gold/15591. MooTube （Silver）/MooTube （Silver）.py
+++ 백준/Gold/15591. MooTube （Silver）/MooTube （Silver）.py
@@ -10,38 +10,37 @@ N, Q = map(int, input().split())
 graph = [[] for _ in range(N+1)]
 for _ in range(N-1):
     p, q, r = map(int, input().split())
+
     graph[p].append([q, r])
     graph[q].append([p, r])
 
 INF = 1e9
+for i in range(Q):
+    answer = 0
 
-# 모든 정점 사이의 USADO 구하기
-usado = [[INF] * (N + 1) for _ in range(N + 1)]
-for i in range(1, N + 1):
+    k, v = map(int, input().split())
+
+    # USADO 중 최솟값 저장
+    usado = [INF] * (N+1)
+
     q = deque()
-    q.append(i)  # 시작 동영상
+    q.append(v)     # 시작 동영상
 
     while q:
         e = q.popleft()
 
         for g in graph[e]:
             # g[0]: 연결된 동영상, g[1]: USADO 값
-            if g[0] == i:  # 시작 동영상인 경우 넘어감
+            if g[0] == v:   # 시작 동영상인 경우 넘어감
                 continue
 
             # 방문하지 않았던 동영상인 경우
-            if usado[i][g[0]] == INF:
+            if usado[g[0]] == INF:
                 # USADO 중 최솟값 저장
-                usado[i][g[0]] = min(g[1], usado[i][e])
+                usado[g[0]] = min(g[1], usado[e])
                 q.append(g[0])
-    usado[i][i] = 0
 
-for i in range(Q):
-    k, v = map(int, input().split())
-
-    answer = 0
-    for j in range(1, N+1):
-        if usado[v][j] >= k:
-            answer += 1
+                if usado[g[0]] >= k:
+                    answer += 1
 
     print(answer)
