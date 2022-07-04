7642c39f5031c034a0be4895cc6500072edd3ba4
류정민 <37644126+charmdew@users.noreply.github.com>
2022-07-04 16:10:08 +0900

    [Gold II] Title: 새로운 게임, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Gold/17780. 새로운 게임/새로운 게임.py
@@ -0,0 +1,88 @@
+import sys
+
+input = sys.stdin.readline
+
+# 체스판의 크기, 말의 개수
+N, K = map(int, input().split())
+
+board = [list(map(int, input().split())) for _ in range(N)]     # 체스판 색 정보
+pieces = [[] for i in range(K)]     # 이동할 말의 좌표, 방향 저장
+
+info = [[[] for _ in range(N)] for _ in range(N)]   # 체스판 말 정보
+for i in range(K):
+    x, y, d = map(int, input().split())
+    if not info[x-1][y-1]:
+        pieces[i] = [x-1, y-1, d%4]
+    info[x-1][y-1].append([i + 1, d%4])
+
+# 하(4), 우(1), 좌(2), 상(3)
+dx = [1, 0, 0, -1]
+dy = [0, 1, -1, 0]
+
+def move(board, pieces, info, N, K):
+    # 1번 말부터 K번 말까지 순서대로 이동
+    for i in range(K):
+        if len(pieces[i])<1:
+            continue
+
+        # i+1 번째 말의 위치
+        x, y, d = pieces[i]
+
+        # 이동하려는 칸
+        nx = x+dx[d]
+        ny = y+dy[d]
+
+        # 파란색 or 벗어나는 경우
+        if (nx<0 or nx>=N or ny<0 or ny>=N) or board[nx][ny]==2:
+            # 이동 방향 반대로
+            d = (d-1)%4 if d%2==0 else (d+1)%4
+
+            nx = x+dx[d]
+            ny = y+dy[d]
+
+            info[x][y][0][1] = d    # 방향 변경
+            # 파란색 or 벗어나는 경우
+            if (nx < 0 or nx >= N or ny < 0 or ny >= N) or board[nx][ny] == 2:
+                # 이동 안하고 방향만 바꾸기
+                pieces[i] = [x, y, d]
+                continue
+
+        # 빨간색
+        if board[nx][ny]==1:
+            # 그 말과 그 위에 있는 모든 말의 쌓여있는 순서 반대로
+            info[x][y] = info[x][y][::-1]
+            pieces[i] = []
+
+            # 기존에 말이 있는 경우
+            if info[nx][ny]:
+                pieces[info[x][y][0][0]-1] = []
+            else:
+                pieces[info[x][y][0][0]-1] = [nx, ny, info[x][y][0][1]]
+
+            info[nx][ny].extend(info[x][y])  # 기존에 있는 말 위에 쌓기
+            info[x][y] = []
+
+        # 흰색
+        if board[nx][ny]==0:
+            # 기존에 말이 있는 경우
+            if info[nx][ny]:
+                pieces[i] = []
+            else:
+                pieces[i] = [nx, ny, d]
+
+            info[nx][ny].extend(info[x][y])   # 기존에 있는 말 위에 쌓기
+            info[x][y] = []
+
+        # 말이 4개 이상 쌓이는 경우 종료
+        if len(info[nx][ny]) >= 4:
+            return True
+
+    return False
+
+
+answer = -1
+for turn in range(1, 1001):
+    if move(board, pieces, info, N, K):
+        answer = turn
+        break
+print(answer)
