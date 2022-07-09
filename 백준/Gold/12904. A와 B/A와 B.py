9fdd5af0866fe47d1f989db8dc1bbabdf34fb27c
류정민 <37644126+charmdew@users.noreply.github.com>
2022-07-09 16:26:11 +0900

    [Gold V] Title: A와 B, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Gold/12904. A와 B/A와 B.py
@@ -0,0 +1,20 @@
+"""
+두 가지 연산만 가능
+1. 문자열의 뒤에 A를 추가한다.
+2. 문자열을 뒤집고 뒤에 B를 추가한다.
+"""
+
+import sys
+
+input = sys.stdin.readline
+
+S = input().rstrip()
+T = input().rstrip()
+
+while len(S) != len(T):
+    if T[-1] == 'A':
+        T = T[:-1]
+    else:
+        T = T[::-1][1:]
+
+print(int(S==T))
