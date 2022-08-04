dba71cdeb32d83d9b4261633091b709ba4b50bfc
류정민 <37644126+charmdew@users.noreply.github.com>
2022-08-05 00:55:16 +0900

    [Silver I] Title: RGB거리, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,22 +1,21 @@
 import sys
-input= sys.stdin.readline
 
-# 집의 수 N(2 ≤ N ≤ 1,000) 입력
+input = sys.stdin.readline
+
 N = int(input())
 
-# 각 집을 빨강, 초록, 파랑으로 칠하는 비용 입력
-cost= [list(map(int, input().split())) for _ in range(N)]
+home = [[0]*3 for i in range(N+1)]
+home[0] = [0, 0, 0]
 
-# print(cost)
+for i in range(1, N+1):
+    # i-1번째 집을 빨강, 초록, 파랑으로 칠하는 비용
+    preR, preG, preB = home[i - 1]
+    
+    # i번째 집을 빨강, 초록, 파랑으로 칠하는 비용
+    r, g, b = map(int, input().split())
 
-color =[[1, 2], [0, 2], [0, 1]]
-ans = [[0]*3 for _ in range(N)]
+    home[i][0] = r + min(preG, preB)
+    home[i][1] = g + min(preR, preB)
+    home[i][2] = b + min(preR, preG)
 
-ans[0] = cost[0]
-for i in range(1, N):
-  for j in range(3):
-    # print(color[j][0], color[j][1])
-    ans[i][j]= cost[i][j]+min(ans[i-1][color[j][0]], ans[i-1][color[j][1]])        
-
-# print(ans)
-print(min(ans[N-1]))
+print(min(home[N]))
