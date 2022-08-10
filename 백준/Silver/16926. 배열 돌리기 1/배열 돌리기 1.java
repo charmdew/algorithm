8665d1dc82d5cad7ec9a35929abb1452108784a5
류정민 <37644126+charmdew@users.noreply.github.com>
2022-08-10 13:23:29 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 1660 ms, Memory: 35888 KB -BaekjoonHub

--- 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
@@ -15,7 +15,6 @@ public class Main {
 		int R = Integer.parseInt(st.nextToken());
 		
 		int[][] arr = new int[N][M];
-		int[][] newArr = new int[N][M];
 		
 		for(int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
@@ -23,47 +22,37 @@ public class Main {
 				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-//		printInfo(arr, N, M);
-		
+
+		// 테두리 개수
 		int bound = Math.min(N, M)/2;
 		
 		int cx, cy, px, py;
-		int start, n, m;
+		int start, n, m, l;
 		
 		int[][] dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 		
+		// R번 회전
 		for(int r=0; r<R; r++) {
+			// 테두리마다 반복
 			for(int i=0; i<bound; i++) {
-				start = arr[i][i];
-				n = N-1-2*i;
-				m = M-1-2*i;
+				start = arr[i][i]; // 처음 시작 위치 값 저장
+				n = N-1-2*i;	// 테두리 세로 길이-1
+				m = M-1-2*i;	// 테두리 가로 길이-1
 				
-				cx = i;
-				cy = i;
+				cx = i; cy = i;
+				// 테두리를 →, ↓, ←, ↑ 순으로 방문하여 반시계 방향으로 돌려줌 
 				for(int d=0; d<4; d++) {
-					if (d%2==0) {
-						for(int j=0; j<m; j++) {
-							px = cx;
-							py = cy;
-							
-							cx += dir[d][0];
-							cy += dir[d][1];
-//							System.out.println(px+", "+py+"<-"+ cx+", "+cy);
-							
-							arr[px][py] = arr[cx][cy];
-						}
-					}
+					l =(d%2==0)? m:n; 
 					
-					else {
-						for(int j=0; j<n; j++) {
-							px = cx;
-							py = cy;
-							
-							cx += dir[d][0];
-							cy += dir[d][1];
-//							System.out.println(px+", "+py+"<-"+ cx+", "+cy);
-							arr[px][py] = arr[cx][cy];
-						}
+					for(int j=0; j<l; j++) {
+						px = cx;
+						py = cy;
+						
+						cx += dir[d][0];
+						cy += dir[d][1];
+
+						// 반시계 방향으로 돌림
+						arr[px][py] = arr[cx][cy];
 					}
 				}
 				arr[i+1][i] = start;
