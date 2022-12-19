1abe7672152be735152ae63a52dca5d8a5a40d4b
류정민 <37644126+charmdew@users.noreply.github.com>
2022-12-19 15:42:35 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,30 +1,42 @@
 def solution(id_list, report, k):
-    answer = []
+    # ============ 간단한 코드 ====================
+    answer = [0] * len(id_list)    
+    reports = {x : 0 for x in id_list}
+
+    for r in set(report):
+        reports[r.split()[1]] += 1
+
+    for r in set(report):
+        if reports[r.split()[1]] >= k:
+            answer[id_list.index(r.split()[0])] += 1
+    # ============================================
+    
+#     answer = []
      
-    # 유저가 신고한 ID 정보
-    report_info = {id:[] for id in id_list}
+#     # 유저가 신고한 ID 정보
+#     report_info = {id:[] for id in id_list}
     
-    # 각 유저별 신고당한 횟수
-    report_cnt = {id:0 for id in id_list}
+#     # 각 유저별 신고당한 횟수
+#     report_cnt = {id:0 for id in id_list}
     
-    # 각 유저가 받은 결과 메일 수
-    result = {id:0 for id in id_list}
+#     # 각 유저가 받은 결과 메일 수
+#     result = {id:0 for id in id_list}
     
-    for x in report:
-        user, to = x.split()
+#     for x in report:
+#         user, to = x.split()
         
-        if to not in report_info[user]:
-            report_info[user].append(to)
+#         if to not in report_info[user]:
+#             report_info[user].append(to)
         
-            # 신고당한 횟수 증가
-            report_cnt[to]+= 1
+#             # 신고당한 횟수 증가
+#             report_cnt[to]+= 1
     
-    for key, value in report_info.items():
-        for v in value:
-            # 정지당한 ID인 경우
-            if report_cnt[v]>=k:
-                result[key]+=1
+#     for key, value in report_info.items():
+#         for v in value:
+#             # 정지당한 ID인 경우
+#             if report_cnt[v]>=k:
+#                 result[key]+=1
     
-    answer = list(result.values())
+#     answer = list(result.values())
     
     return answer
