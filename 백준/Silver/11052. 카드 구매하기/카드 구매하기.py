202dc72edb3dd3ba8043a4ea7d14be84831efadf
류정민 <37644126+charmdew@users.noreply.github.com>
2023-01-14 15:16:46 +0900

    [Silver I] Title: 카드 구매하기, Time: 128 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -11,6 +11,12 @@ P = [0]+[*map(int, input().split())]
 # dp[i] : 카드 i개를 갖기 위해 지불해야 하는 금액 최댓값
 dp = [*P]
 
+"""
+N   = 1 + N-1
+    = 2 + N-2
+    ...
+    = N//2 + N-(N//2)
+"""
 for i in range(1, N+1):
     for j in range(1, i//2+1):
         dp[i] = max(dp[j]+dp[i-j], dp[i])
