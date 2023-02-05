f137ea846c27c883380af59f66c5dea4232c06c8
류정민 <37644126+charmdew@users.noreply.github.com>
2023-02-05 14:49:49 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,24 +1,39 @@
-def solution(bridge_length, weight, truck_weights):
+from collections import deque
 
-    crossing = []   # 다리를 건너는 트럭 저장
-    time, w = 0, 0  # 경과 시간, 현재 다리에 있는 트럭의 무게 합
+def solution(bridge_length, weight, truck_weights):
+    answer = 0
+
+    # 대기 트럭
+    waiting = deque(truck_weights)
     
-    while truck_weights:
-        time += 1
+    # 경과 시간
+    t = 0
+    
+    # 다리를 건너는 트럭
+    crossing = deque()
+    
+    # 다리에 있는 무게 합
+    weight_sum = 0
+    
+    while waiting:
+        # 경과 시간 +1
+        t += 1
         
-        # 지나간 트럭 빼기
-        if crossing and crossing[0][1] == time:
-            tw, t = crossing.pop(0)
-            w -= tw
-        
-        # weight 이하까지의 무게, 트럭이 최대 bridge_length대 올라갈 수 있음
-        if w+truck_weights[0] <=weight and len(crossing)+1 <=bridge_length:
-            t = truck_weights.pop(0)    # 첫번째 대기 트럭 꺼냄
-            w += t          
-            # 다리를 건너는 트럭에 추가 [무게, 다리를 다 건너는 시간]
-            crossing.append([t, time+bridge_length])
+        # 다리를 건넌 트럭 처리
+        if crossing and crossing[0][1] + bridge_length == t:
+            c = crossing.popleft()
+            weight_sum -= c[0] 
+                
+        # 무게 합이 weight을 넘지 않는다면
+        if weight_sum + waiting[0] <= weight:
+            # 다리를 건넘
+            truck = waiting.popleft() # 대기 트럭에서 제거
             
+            crossing.append([truck, t]) # 다리를 건너는 트럭 목록에 추가
+            
+            weight_sum += truck # 다리를 건너는 트럭의 무게 합
+                
     # 마지막 트럭이 다리를 건너기 시작한 시간 + 다리 길이    
-    answer = time + bridge_length
-    
+    answer = t + bridge_length
+        
     return answer
