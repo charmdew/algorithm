f9af3d7572d2e9bc859124df7101c8f7c82144ce
류정민 <37644126+charmdew@users.noreply.github.com>
2023-02-12 12:18:03 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.py
@@ -1,36 +1,40 @@
-"""
-장르별로 가장 많이 재생된 노래를 최대 두 개까지 모아 베스트 앨범 출시
-"""
-from collections import defaultdict
 import heapq
 
 def solution(genres, plays):
     answer = []
     
-    album = defaultdict(list)  # 장르, 노래별 재생 수 저장
-    pcnt = defaultdict(int)   # 장르에 속한 노래들 재생 수 저장
+    # 노래 개수
+    N = len(plays)
     
-    for i, genre in enumerate(genres):
-        pcnt[genre] += plays[i]
-
-        # 큐에 원소 1개 이하 일때는 무조건 큐에 넣기
-        if len(album[genre])<=1:
-            heapq.heappush(album[genre], [plays[i], i])
-
-        # 더 많이 재생된 노래일 경우 큐에 넣기
-        elif plays[i] > album[genre][0][0]:
-            heapq.heappop(album[genre])
-            heapq.heappush(album[genre], [plays[i], i])
-
+    # 장르별 재생횟수 저장
+    genres_cnt = {g:0 for g in set(genres)}
+    # 장르별 노래 정보 저장 [재생횟수, 고유번호]
+    genres_detail = {g:[] for g in set(genres)}
+    
+    for i in range(N):
+        genre, play = genres[i], plays[i]
+        
+        # 장르별 재생횟수 저장
+        genres_cnt[genre] += play
+    
+        # 장르 별 가장 많이 재생된 노래 두 개씩 저장
+        if len(genres_detail[genre]) < 2:
+            # 노래가 두 개 미만인 경우는 그대로 추가
+            heapq.heappush(genres_detail[genre], [play, -i])
+        else:
+            play_cnt, num = genres_detail[genre][0]
+            # 재생 횟수가 더 많거나, 재생 횟수가 같은데 고유 번호가 낮은 노래인 경우
+            heapq.heappushpop(genres_detail[genre], [play, -i])
+    
     # 속한 노래가 많이 재생된 장르 순으로 정렬
-    sg = sorted(pcnt.items(), key=lambda x: -x[1])
-    
-    for genre, cnt in sg:
-        # 장르 내에서 많이 재생된 노래 순 
-        # 재생 횟수가 같은 노래 중에서는 고유 번호가 낮은 순
-        sl = sorted(album[genre], key=lambda x: [-x[0], x[1]])
-        # print(sl)
-        for play, idx in sl:
-            answer.append(idx)
-    
+    genres_cnt = list(genres_cnt.items())
+    genres_cnt.sort(key=lambda x:-x[1])
+
+    for genre, cnt in genres_cnt:
+        songs = genres_detail[genre][::-1]
+
+        # 장르 내에서 많이 재생된 순으로 저장
+        for song in songs:
+            answer.append(-song[1])
+        
     return answer
