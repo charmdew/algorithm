5adfa0ae49fa8566a2c596cdc69b52edf09c9f39
류정민 <37644126+charmdew@users.noreply.github.com>
2023-02-26 21:27:17 +0900

    [level 3] Title: 불량 사용자, Time: 6.41 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.py
@@ -1,22 +1,37 @@
+"""
+정규표현식 사용
+- 정규식 "a.b" => "a + 모든문자 + b"
+"""
+
+
 import re
 from itertools import permutations 
 
 def solution(user_id, banned_id):
     n = len(banned_id)
+    
+    # 불량 사용자 아이디의 *를 .로 변경
     banned_id = [i.replace("*", ".") for i in banned_id]
+    
+    # 제재 아이디 목록 저장
     answer = []
 
-    for i in permutations(user_id, n):
-        lst = list(i)
+    # 모든 순열 조합 확인
+    for case in permutations(user_id, n):
+        users = list(case)
         flag = True
+        
         for j in range(n):
-            if re.match(banned_id[j], lst[j]) and (len(banned_id[j]) == len(lst[j])) :
+            # 불량 사용자 목록에 매핑됨
+            # 길이 체크 필요! ("acbc"도 "a.b"랑 match 한다고 나옴)
+            if re.match(banned_id[j], users[j]) and (len(banned_id[j]) == len(users[j])):
                 continue 
+            # 불량 사용자 목록에 매핑 안됨
             else:
                 flag = False
                 break
         if flag:
-            if sorted(lst) not in answer:
-                answer.append(sorted(lst))
+            if sorted(users) not in answer:
+                answer.append(sorted(users))
 
     return len(answer) 
