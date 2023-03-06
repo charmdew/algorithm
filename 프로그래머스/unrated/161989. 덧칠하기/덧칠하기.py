e035aa1ca7bb0eb693410ef1c8dc990ae681db6f
류정민 <37644126+charmdew@users.noreply.github.com>
2023-03-06 21:22:58 +0900

    [unrated] Title: 덧칠하기, Time: 3.41 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.py
@@ -3,14 +3,17 @@ import math
 def solution(n, m, section):
     answer = 1
     
+    # 페인트를 칠하기로 정한 구역 번호 수
     L = len(section)
     
+    # 롤러로 페인트칠하는 시작점 저장
     pre = section[0]
     for i in range(1, L):
+        # 롤러로 페인트칠을 한 구역 내에 있다면 넘어감
         if pre + m > section[i]:
             continue
         
-        # 페인트칠 +1
+        # 롤러로 페인트칠하는 횟수 추가
         answer += 1
         pre = section[i]
     
