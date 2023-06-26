f57613f6f07c5869de7e1f6ed8eeb0fb32046afa
류정민 <37644126+charmdew@users.noreply.github.com>
2023-06-26 23:32:30 +0900

    [level 2] Title: 숫자 블록, Time: 5278.87 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12923. 숫자 블록/숫자 블록.py
+++ 프로그래머스/lv2/12923. 숫자 블록/숫자 블록.py
@@ -1,13 +1,23 @@
-"""계속 시간초과... 풀이 찾아봄!"""
+# 해당 숫자의 약수 중에 자기 자신을 제외하고 가장 큰 수 구하기
+def divisor(n):
+    if n==1:
+        return 0
+    
+    block = 1
+    for i in range(2, int(n**(1/2)+1)):
+        if n%i == 0:
+            # 1부터 10,000,000까지의 숫자가 적힌 블록 사용
+            if n//i > 10000000:
+                block = max(block, i)
+                continue
+            block = max(block, n//i)
+    return block
+
 
 def solution(begin, end):
     answer = []
-    for i in range(begin, end+1):
-        k = int(i != 1)  # i가 1이면 0, 1이 아니면 1을 대입
-        for j in range(2, int(i**0.5)+1): # i**0.5 == sqrt(i)
-            if i%j == 0 and i//j <= 10000000:
-                k = i//j  # j가 2부터 커지기 때문에 처음 만나는 i//j가 약수 중 가장 큰 값
-                break
-        answer.append(k)
+    
+    for x in range(begin, end+1):
+        answer.append(divisor(x))
     
     return answer
