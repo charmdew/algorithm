ed238e5f807ea0ba7ca001b402a35ffb02a7202d
류정민 <37644126+charmdew@users.noreply.github.com>
2023-07-20 12:12:52 +0900

    [Gold III] Title: 파티, Time: 64 ms, Memory: 34348 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -1,20 +1,22 @@
-import sys 
+import sys
 import heapq
 
-input = sys.stdin.readline 
+input = sys.stdin.readline
 
-# N, M, X 입력
 N, M, X = map(int, input().split())
 
-# 도로 정보 입력
-graph = [[] for _ in range(N+1)]
+fgraph = [[] for _ in range(N+1)]
+# 역방향 저장
+rgraph = [[] for _ in range(N+1)]
 
 for _ in range(M):
     a, b, w = map(int, input().split())
-    graph[a].append((b, w))
 
-# 다익스트라 이용
-def dijkstra(start):
+    fgraph[a].append((b, w))
+    rgraph[b].append((a, w))
+
+
+def dijkstra(start, graph):
     q = []
     heapq.heappush(q, (0, start))
 
@@ -23,7 +25,6 @@ def dijkstra(start):
 
         if time[now] < t:
             continue
-            
         for x, tt in graph[now]:
             cost = time[now] + tt
 
@@ -31,22 +32,18 @@ def dijkstra(start):
                 time[x] = cost
                 heapq.heappush(q, (cost, x))
 
-# 학생들이 오고가는데 걸리는 최단 시간 저장
-result = [0] * (N+1)
 
-# 1~N에서 X까지 가는 최단 시간 구하기
-for i in range(1, N+1):
-    time = [1e9] * (N+1)
-    time[i] = 0
-    dijkstra(i)
-    result[i] = time[X]
-
-# X에서 1~N까지 가는 최단 시간 구하기
-time = [1e9] * (N+1)
+# 다익스트라 수행 (각 마을 ~ X 마을까지의 최단 소요시간)
+time = [1e9]*(N+1)
 time[X] = 0
-dijkstra(X)
+dijkstra(X, rgraph)
+result = [0] + [time[i] for i in range(1, N+1)]
+
+# 다익스트라 수행 (X 마을 ~ 각 마을까지의 최단 소요시간)
+time = [1e9]*(N+1)
+time[X] = 0
+dijkstra(X, fgraph)
 for i in range(1, N+1):
     result[i] += time[i]
 
-# 가장 오래 걸리는 학생의 소요시간
 print(max(result))
