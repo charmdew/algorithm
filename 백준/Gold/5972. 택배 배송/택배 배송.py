4c0885afa7ae3956e4c07ffd639b51ebbaebf66d
류정민 <37644126+charmdew@users.noreply.github.com>
2023-07-25 15:48:11 +0900

    [Gold V] Title: 택배 배송, Time: 260 ms, Memory: 51560 KB -BaekjoonHub

---
+++ 백준/Gold/5972. 택배 배송/택배 배송.py
@@ -0,0 +1,35 @@
+# 다익스트라
+# 1~N까지 가는 최소비용 구하기
+
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+graph = [[] for _ in range(N+1)]
+for _ in range(M):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+    graph[b].append((a, c))
+
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+
+    while q:
+        c, now = heapq.heappop(q)
+
+        for x, cc in graph[now]:
+            nc = c + cc
+
+            if nc < cost[x]:
+                cost[x] = nc
+                heapq.heappush(q, (nc, x))
+
+
+cost = [1e9] * (N+1)
+dijkstra(1)
+print(cost[N])
