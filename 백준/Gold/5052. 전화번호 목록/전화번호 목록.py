09be26cef444610811ac6bb6c52b9d542b2f171b
류정민 <37644126+charmdew@users.noreply.github.com>
2023-08-11 00:20:09 +0900

    [Gold IV] Title: 전화번호 목록, Time: 176 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
@@ -1,28 +1,23 @@
-import sys
+"""
+다른사람 풀이
+- 실행 시간 더 짧음!
+"""
 
-input = sys.stdin.readline
-
-for t in range(int(input())):
-    n = int(input())
-
-    phone_nums = [input().rstrip() for _ in range(n)]
-    phone_nums.sort(key=lambda x: len(x))
-
-    prefix = dict()
-    consistency = True
-
-    for phone_num in phone_nums:
-        # 다른 번호가 현재 번호의 접두어인지 확인
-        for i in range(1, len(phone_num)+1):
-            # 이미 있는 경우 일관성 유지 못함
-            if phone_num[:i] in prefix:
-                consistency = False
-                break
-
-        # 현재 전화번호 접두어로 추가
-        prefix[phone_num[:i]] = 1
-
-        if not consistency:
+from sys import stdin
+input=stdin.readline
+n=int(input())
+for i in range(n):
+    m=int(input())
+    L=[]
+    for j in range(m):
+        L.append(input().rstrip())
+    L.sort()
+    fin=False
+    for j in range(m-1):
+        if L[j]==L[j+1][:len(L[j])]:
+            fin=True
             break
-
-    print("YES" if consistency else "NO")
+    if fin:
+        print("NO")
+    else:
+        print("YES")
