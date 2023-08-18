2f68cd7184ef2d8e873f26f0a9c55b527bb78411
류정민 <37644126+charmdew@users.noreply.github.com>
2023-08-18 22:19:40 +0900

    [Gold V] Title: 동전 2, Time: 196 ms, Memory: 32036 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -0,0 +1,16 @@
+import sys
+
+n, k = map(int, input().split())
+
+dp = [1e9] * (100001)
+
+for _ in range(n):
+    coin = int(input())
+
+    dp[coin] = 1
+
+    for x in range(coin+1, k+1):
+        if dp[x-coin] + 1 < dp[x]:
+            dp[x] = dp[x-coin] + 1
+
+print(dp[k] if dp[k] < 1e9 else -1)
