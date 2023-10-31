8374e09cbbfcc7cc058163badb975f757f3fd523
류정민 <37644126+charmdew@users.noreply.github.com>
2023-10-31 10:56:53 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 760 ms, Memory: 93316 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -7,23 +7,23 @@ S = input().rstrip()
 n = len(S)
 
 # dp[i][j]
-# i : 알파벳 소문자 아스키코드값 - 'a' 아스키코드값
-# dp[i][j]: 0번째 문자부터 j번째 문자까지 알파벳 등장 횟수 저장
-dp = [[0]*n for _ in range(26)]
+# j : 알파벳 소문자 아스키코드값 - 'a' 아스키코드값
+# dp[i][j]: 0번째 문자부터 i번째 문자까지 알파벳 등장 횟수 저장
+dp = [[0]*26]
 ord_a = ord('a')
+dp[0][ord(S[0])-ord_a] = 1
 
-for x in range(n):
-    dp[ord(S[x])-ord_a][x] = 1
-for i in range(26):
-    for j in range(1, n):
-        dp[i][j] += dp[i][j-1]
+for x in range(1, n):
+    dp.append(dp[-1][:])
+    dp[x][ord(S[x])-ord_a] += 1
 
 for _ in range(int(input())):
     a, l, r = input().split()
+    idx = ord(a)-ord_a
     r = int(r)
     l = int(l)
 
     if l == 0:
-        print(dp[ord(a)-ord_a][r])
+        print(dp[r][idx])
     else:
-        print(dp[ord(a)-ord_a][r]-dp[ord(a)-ord_a][l-1])
+        print(dp[r][idx]-dp[l-1][idx])
