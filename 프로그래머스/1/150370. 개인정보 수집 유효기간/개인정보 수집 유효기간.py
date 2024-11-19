8852982b8a55805931c8b428d77396da7a12e24b
류정민 <37644126+charmdew@users.noreply.github.com>
2024-11-19 22:09:48 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.24 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,23 +1,12 @@
+def to_days(date):
+    year, month, day = map(int, date.split("."))
+    return year * 28 * 12 + month * 28 + day
+
 def solution(today, terms, privacies):
-    answer = []
-    
-    # 일수로 계산
-    # 연 : 12*28 = 336 , 월 : 28, 일 : 1
-    today_arr = list(map(int, today.split('.')))
-    today_num = today_arr[0]*336 + today_arr[1]*28 + today_arr[2]
-    
-    # 약관 종류별 보관 가능 기준일자 일수로 변환하여 저장
-    terms_dict = dict()
-    for term in terms:
-        k, m = term.split()
-        terms_dict[k] = today_num - int(m)*28
-    
-    for i, privacy in enumerate(privacies):
-        date, kind = privacy.split()
-        arr = list(map(int, date.split('.')))
-        num = arr[0]*336 + arr[1]*28 + arr[2]
-        
-        if num <= terms_dict[kind]:
-            answer.append(i+1)
-    
-    return answer
+    months = {v[0]: int(v[2:]) * 28 for v in terms}
+    today = to_days(today)
+    expire = [
+        i + 1 for i, privacy in enumerate(privacies)
+        if to_days(privacy[:-2]) + months[privacy[-1]] <= today
+    ]
+    return expire
